0fdf5d699c01eac62daf6d580779daec029f4573
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-07-26 01:23:16 +0800

    ui improved

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -2,7 +2,7 @@ import Note from "../models/notesSchema.js"
 import express from "express";
 const router = express.Router();
 
-router.get("/notes",async(req,res)=>{
+router.get("/",async(req,res)=>{
     let notes = await Note.find()
     res.render("notes", {notes})
 } )
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ configureViewEngine(app)
 const PORT = process.env.PORT
 
 app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
 app.use(express.json());
 app.use("/", apiRoutes)
 app.use("/", pageRoutes)
